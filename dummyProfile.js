e7c94656d06694de3a8e32446fd86427707fd388
mrhouzlane <mrhouzlane@github.com>
2022-08-07 13:18:19 +0200

    profiles

--- dummyProfile.js
+++ dummyProfile.js
@@ -9,7 +9,7 @@ let nordimukiele = {
 
 let JohnnyDrille = {
     address: "0x90f79bf6eb2c4f870365e785982e1f101e93b906",
-    username : "Johnny Drille",
+    username : "Johnny_Drille",
     avatar : "https://bafkreiddqz53d7422nft6zgn62bzwz44o7tla2rzvevhvv6deeuo5jqz7e.ipfs.nftstorage.link/",
     banner : "ipfs://bafkreibc2o6emeuvftvqhbf7p5y2klkjqaj6ryfpcwcr6tpbduui6bc4oy",
     bio: "Before We Fall Asleep ",
@@ -18,7 +18,7 @@ let JohnnyDrille = {
 
 let andrewTate = {
     address: "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65",
-    username : "Andrew Tate",
+    username : "AndrewTate",
     avatar : "https://bafybeieir6ckk2qlou3ld43ttvlqw7jgqdleexrb6km5qnnct2utukvjpu.ipfs.nftstorage.link/",
     banner : "https://bafkreidv2bpbuyb5ci6tdxetmtjx2trhrsl36lhai5iwwqvqiy5pw5yn5u.ipfs.nftstorage.link/",
     bio: "Fastest is winner",
@@ -28,105 +28,46 @@ let andrewTate = {
 
 let PatrickCollins = {
     address: "0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc",
-    username : "Patrick Collins",
+    username : "Patrickins",
     avatar : "https://bafkreihhylx4muzz6xprak5ova5tr7f3ncmfhfa447u4wjpkzzyq2rw5ja.ipfs.nftstorage.link/",
     banner : "https://bafkreidn3rn6ztod4uvngedj4h77q4fj6x5kxc6iictdsdfegtkjjkwzia.ipfs.nftstorage.link/",
     bio: "BlockEnd",
     links : [ ["Instagram", "https://www.instagram.com/_patrick.collins/"], ["Twitter", "https://twitter.com/PatrickAlphaC"]] 
 }
 
-let  = {
+let CarlaMoreau = {
     address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
+    username : "Carlamu",
+    avatar : "https://bafkreig3oaobxpkqeyosq3mb7nxq7phibaq3xjoivyqvwhtnjceas7w46y.ipfs.nftstorage.link/",
+    banner : "https://bafkreig3oaobxpkqeyosq3mb7nxq7phibaq3xjoivyqvwhtnjceas7w46y.ipfs.nftstorage.link/",
+    bio: "MOM OF rubyguedj ",
+    links : [ ["Instagram", "https://www.instagram.com/carlamoreau_____/"], ["Twitter", "https://twitter.com/carlamoreau____"]] 
 }
 
-let  = {
+let MatthewMcConaughey = {
     address: "0x14dc79964da2c08b23698b3d3cc7ca32193d9955",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
+    username : "Mattheyyy",
+    avatar : "https://bafkreiaknkmdpi2azploxztmvq3r2cexorjqbvnvdc656aturswmoprxye.ipfs.nftstorage.link/",
+    banner : "https://bafkreih3c3ncqhysjbwb5c6f2qqgligxfeo7ahqxro4k5v5muve4usfcom.ipfs.nftstorage.link/",
+    bio: "sometimes you have etc. sometimes etc. has you",
+    links : [ ["Instagram", "https://www.instagram.com/officiallymcconaughey/"], ["Twitter", "https://twitter.com/McConaughey"]] 
 }
 
-let  = {
+let Max  = {
     address: "0x23618e81e3f5cdf7f54c3d65f7fbc0abf5b21e8f",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
+    username : "Max",
+    avatar : "https://bafkreibhj2oiuxgig54zewojqmqchp4zahu5i5fjgfpqfgwgdp56x4taku.ipfs.nftstorage.link/",
+    banner : "https://bafkreigxzuchftlnjjpnz7mjxiza5srvore5y72an4rvu3xhhrebvnvrwi.ipfs.nftstorage.link/",
+    bio: "cutie pie",
+    links : [ ["Instagram", "https://www.instagram.com/dogs.lovers/"], ["Twitter", "https://twitter.com/Crazyrottweile1"]] 
 }
 
-let  = {
+let AlixBénézech = {
     address: "0xa0ee7a142d267c1f36714e4a8f75612f20a79720",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
+    username : "Alixbch",
+    avatar : "https://bafkreidbzu6wvaxknd552p7qscuv7y2ytrljhqv33plnwphla5po2vijle.ipfs.nftstorage.link/",
+    banner : "https://bafkreif3b72suaf7sjm2qc2tphkdbfy32qblgo4wb3kw65cbxfkk7hcvja.ipfs.nftstorage.link/",
+    bio: "acting is not faking",
+    links : [ ["Instagram", "https://www.instagram.com/alixbenezech/"], ["Twitter", "https://twitter.com/alixbenezech"]] 
 }
-
-let  = {
-    address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
-}
-
-let  = {
-    address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
-}
-
-let  = {
-    address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
-}
-
-let  = {
-    address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
-}
-
-let  = {
-    address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
-}
-
-let  = {
-    address: "0x976ea74026e726554db657fa54763abd0c3a0aa9",
-    username : "",
-    avatar : "",
-    banner : "",
-    bio: "",
-    links : [ ["Instagram", ""], ["Twitter", ""]] 
-}
-
-
-
-
-
 
