fa001cb995eb9f3d10c0360980afef0cee0bcd08
mrhouzlane <mrhouzlane@github.com>
2022-08-07 15:14:23 +0200

    zoraApi

--- zoraApi.js
+++ zoraApi.js
@@ -1,49 +1,23 @@
-import { ZDK, ZDKNetwork, ZDKChain } from "@zoralabs/zdk";
+import { request, gql } from 'graphql-request'
 
-const networkInfo = {
-    network: ZDKNetwork.Ethereum,
-    chain: ZDKChain.Mainnet,
+
+const profiles = `
+query MyQuery {
+    token(token: {address: "0xB2837f4B89F921101A8098EbfcDc00b6A70E18fc", tokenId: "1"}, network: {chain: RINKEBY}) {
+      token {
+        metadata
+      }
+    }
   }
-
-const API_ENDPOINT = "https://api.zora.co/graphql";
-
-
-let arrayOfArgs = []
-
-let arrayOfTokens = [
-    [
-        "0x8d04a8c79cEB0889Bdd12acdF3Fa9D207eD3Ff63", //address
-        "314"        //tokenId 
-    ],
-    [
-        "0x8d04a8c79cEB0889Bdd12acdF3Fa9D207eD3Ff63", //another address 
-        "314"     // another tokenId 
-    ]
-]
-
-for(let i = 0; i < arrayOfTokens.length; i++){//pushing addresses and tokens to args object
-    arrayOfArgs.push({
-        token: {
-            address: arrayOfTokens[i][0],
-            tokenId: arrayOfTokens[i][1]
-        },
-        includeFullDetails: false 
-
-    })
-    
-}
+  
+  
+`
 
 
-// RESPONSE 
+var response
 
-const zdk = new ZDK() 
-
-arrayOfArgs.map((args) => {
-    const response = zdk.token(args)
-    console.log(response)
-    response.then(function(result) {
-        console.log(result)
-
-    })
-}) 
+await request('https://api.zora.co/graphql', profiles).then(data => {
+    response = data 
+})
+console.log(response.token.token.metadata)
 
